ddca22d184c3f8c2b540f5441a998fb29e8b0ad9
Tuan  vi <you@example.com>
2023-10-05 23:41:42 +0700

    update user page

---
+++ src/pages/clients/User/UserInfo.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./style.scss";
+import "./index.scss";
+import { Link } from "react-router-dom";
+import NavUser from "./layout/NavUser";
+function UserInfo() {
+  return <NavUser>skdsdksdkskdsdks</NavUser>;
+}
+
+export default UserInfo;
--- src/pages/clients/User/layout/NavUser.tsx
+++ src/pages/clients/User/layout/NavUser.tsx
@@ -1,9 +1,97 @@
 import { Grid } from "@mui/material";
+import { Link } from "react-router-dom";
+import "../style.scss";
+import "../index.scss";
 
-function NavUser() {
-  return <Grid>
-    
-  </Grid>;
+function NavUser({ children }: { children: React.ReactNode }) {
+  return (
+    <Grid>
+      <div className="UserGroup">
+        <section className="user">
+          <div className="container">
+            <div className="row">
+              <div className="wide c-0 m-0 l-3">
+                <div className="sidebar p-3">
+                  <div className="side-waper p-3 p-4">
+                    <h1 className="side-hd pb-3">Tài khoản</h1>
+                    <div className="side-item pt-3 pb-1">
+                      <Link to="">
+                        <p className="side-item-text">
+                          Bảng điều kiển tài khoản
+                        </p>
+                      </Link>
+                    </div>
+                    <div className="side-item pt-3 pb-1">
+                      <Link to="">
+                        <p className="side-item-text">Thông tin tài khoản</p>
+                      </Link>
+                    </div>
+                    <div className="side-item pt-3 pb-1">
+                      <Link to="">
+                        <p className="side-item-text">
+                          Bảng điều kiển tài khoản
+                        </p>
+                      </Link>
+                    </div>
+                    <div className="side-item pt-3 pb-1">
+                      <Link to="">
+                        <p className="side-item-text">
+                          Bảng điều kiển tài khoản
+                        </p>
+                      </Link>
+                    </div>
+                    <div className="side-item pt-3 pb-1">
+                      <Link to="">
+                        <p className="side-item-text">
+                          Bảng điều kiển tài khoản
+                        </p>
+                      </Link>
+                    </div>
+                    <div className="side-item pt-3 pb-1">
+                      <Link to="">
+                        <p className="side-item-text">
+                          Bảng điều kiển tài khoản
+                        </p>
+                      </Link>
+                    </div>
+                    <div className="side-item pt-3 pb-1">
+                      <Link to="">
+                        <p className="side-item-text">
+                          Bảng điều kiển tài khoản
+                        </p>
+                      </Link>
+                    </div>
+                    <div className="side-item pt-3 pb-1">
+                      <Link to="">
+                        <p className="side-item-text">
+                          Bảng điều kiển tài khoản
+                        </p>
+                      </Link>
+                    </div>
+                    <div className="side-item pt-3 pb-1">
+                      <Link to="">
+                        <p className="side-item-text">
+                          Bảng điều kiển tài khoản
+                        </p>
+                      </Link>
+                    </div>
+                    <div className="side-item pt-3 pb-1">
+                      <Link to="">
+                        <p className="side-item-text">
+                          Bảng điều kiển tài khoản
+                        </p>
+                      </Link>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="wide c-12 m-12 l-9">{children}</div>
+            </div>
+          </div>
+        </section>
+      </div>
+    </Grid>
+  );
 }
 
 export default NavUser;
