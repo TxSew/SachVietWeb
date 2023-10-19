893bd3c207439eda555fd52085bb5d6d8ae20fc2
Tuan  vi <you@example.com>
2023-10-19 20:19:16 +0700

    update

--- src/pages/clients/User/Navdasb.tsx
+++ src/pages/clients/User/Navdasb.tsx
@@ -1,5 +1,6 @@
 import { BiBookAdd, BiSolidCategoryAlt } from "react-icons/bi";
 import {
+  FaAngleRight,
   FaBell,
   FaBookOpen,
   FaChevronDown,
@@ -14,69 +15,13 @@ import "./style.scss";
 function NavDas() {
   return (
     <div className="dasb pb-3">
-      <div className="dasb-header">
-        <div className="dasb-header-logo">
-          <h1>TuanDEV</h1>
-        </div>
-        <div className="dasb-header-right row">
-          <div className="dasb-header-togg c-1 m-1 l-1">
-            <NavLink to=""></NavLink>
-          </div>
-          <ul className="dasb-header-right c-11 m-11 l-11">
-            <li className="dasb-header-right-search">
-              <NavLink to="">
-                <i>
-                  <FaSearch />
-                </i>
-              </NavLink>
-            </li>
-            <li className="dasb-header-right-flag">
-              <NavLink to="">
-                <i>
-                  <FaFlag />
-                </i>
-              </NavLink>
-            </li>
-            <li className="dasb-header-right-search">
-              <NavLink to="">
-                <i>
-                  <FaBell />
-                </i>
-              </NavLink>
-            </li>
-            <li className="dasb-header-right-action">
-              <input type="checkbox" id="action" />
-              <label htmlFor="action">
-                <img
-                  width={32}
-                  src="https://php.spruko.com/ynex/ynex/assets/images/faces/9.jpg"
-                  alt="Ảnh admin"
-                />
-                <span>Tuấn</span>
-                <i>
-                  <FaChevronDown />
-                </i>
-              </label>
-              <div className="dasb-header-right-action-drop" id="drop">
-                <NavLink className="info" to="">
-                  <i></i>
-                  <p>Thông tin</p>
-                </NavLink>
-                <NavLink className="log" to="">
-                  <i></i>
-                  <p>Logout</p>
-                </NavLink>
-              </div>
-            </li>
-          </ul>
-        </div>
-      </div>
       <div className="dasb-wapper">
-        <label htmlFor="tool" className="tool">
-          <FaListUl />
-        </label>
         <input type="checkbox" id="tool" />
         <div className="dasb-sidebar">
+          <div className="dasb-sidebar-logo">
+            <h1>MUI</h1>
+          </div>
+
           <ul className="k">
             <li className="">
               <NavLink to="">
@@ -114,7 +59,61 @@ function NavDas() {
           </ul>
         </div>
         <div className="dasb-wapper-main">
-          <div className="dasb-wapper-main-bg"></div>
+          <div className="dasb-header row">
+            <label htmlFor="tool" className="tool">
+              <FaListUl />
+            </label>
+            <ul className="dasb-header-right">
+              <li className="dasb-header-right-search">
+                <NavLink to="">
+                  <i>
+                    <FaSearch />
+                  </i>
+                </NavLink>
+              </li>
+              <li className="dasb-header-right-flag">
+                <NavLink to="">
+                  <i>
+                    <FaFlag />
+                  </i>
+                </NavLink>
+              </li>
+              <li className="dasb-header-right-search">
+                <NavLink to="">
+                  <i>
+                    <FaBell />
+                  </i>
+                </NavLink>
+              </li>
+              <li className="dasb-header-right-action">
+                <input type="checkbox" id="action" />
+                <label htmlFor="action">
+                  <img
+                    width={32}
+                    src="https://php.spruko.com/ynex/ynex/assets/images/faces/9.jpg"
+                    alt="Ảnh admin"
+                  />
+                  <span>Tuấn</span>
+                  <i>
+                    <FaChevronDown />
+                  </i>
+                </label>
+                <div className="dasb-header-right-action-drop" id="drop">
+                  <NavLink className="info" to="">
+                    <i></i>
+                    <p>Thông tin</p>
+                  </NavLink>
+                  <NavLink className="log" to="">
+                    <i></i>
+                    <p>Logout</p>
+                  </NavLink>
+                </div>
+              </li>
+            </ul>
+          </div>
+          <div className="dasb-wapper-main-bg">
+            <div className="content">Khó</div>
+          </div>
         </div>
       </div>
     </div>
