46e81b4b413cdd5ed809697e6600a12b1fa16df1
thanh-ukpc <thanh.do@ukparkingcontrol.com>
2023-10-20 09:22:29 +0700

    implement ui layout admin

--- src/layouts/AdminLayout/AdminLayout.tsx
+++ src/layouts/AdminLayout/AdminLayout.tsx
@@ -2,23 +2,22 @@ import { Grid } from "@mui/material";
 import React from "react";
 import HeaderAdmin from "../components/HeaderAdmin/HeaderAdmin";
 import { NavLink } from "react-router-dom";
-import { FaBookOpen, FaHome, FaListUl, FaWpforms } from "react-icons/fa";
+import { FaBell, FaBookOpen, FaChevronDown, FaFlag, FaHome, FaListUl, FaSearch, FaWpforms } from "react-icons/fa";
 import { BiBookAdd, BiSolidCategoryAlt } from "react-icons/bi";
 
 
 function AdminLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="App dasb pb-3">
-      <HeaderAdmin />
+    <div className="dasb pb-3">
       <div className="dasb-wapper">
-        <label htmlFor="tool" className="tool">
-          <FaListUl />
-        </label>
         <input type="checkbox" id="tool" />
         <div className="dasb-sidebar">
+          <div className="dasb-sidebar-logo">
+            <h1>MUI</h1>
+          </div>
           <ul className="k">
             <li className="">
-              <NavLink to="/admin/statistical">
+              <NavLink to="">
                 <i>
                   <FaHome />
                 </i>
@@ -26,7 +25,7 @@ function AdminLayout({ children }: { children: React.ReactNode }) {
               </NavLink>
             </li>
             <li className="">
-              <NavLink to="/admin/category">
+              <NavLink to="../admin/category">
                 <i>
                   <BiSolidCategoryAlt />
                 </i>
@@ -34,19 +33,11 @@ function AdminLayout({ children }: { children: React.ReactNode }) {
               </NavLink>
             </li>
             <li className="">
-              <NavLink to="/admin/product">
+              <NavLink to="../admin/product">
                 <i>
                   <FaBookOpen />
                 </i>
                 <span>Sản phẩm</span>
-              </NavLink>
-            </li>
-            <li className="">
-              <NavLink to="/admin/orders">
-                <i>
-                  <FaWpforms />
-                </i>
-                <span>Đơn</span>
               </NavLink>
             </li>
             <li className="">
@@ -61,7 +52,61 @@ function AdminLayout({ children }: { children: React.ReactNode }) {
           </ul>
         </div>
         <div className="dasb-wapper-main">
-          <div className="dasb-wapper-main-bg">{children}</div>
+          <div className="dasb-header row">
+            <label htmlFor="tool" className="tool">
+              <FaListUl />
+            </label>
+            <ul className="dasb-header-right">
+              <li className="dasb-header-right-search">
+                <NavLink to="">
+                  <i>
+                    <FaSearch />
+                  </i>
+                </NavLink>
+              </li>
+              <li className="dasb-header-right-flag">
+                <NavLink to="">
+                  <i>
+                    <FaFlag />
+                  </i>
+                </NavLink>
+              </li>
+              <li className="dasb-header-right-search">
+                <NavLink to="">
+                  <i>
+                    <FaBell />
+                  </i>
+                </NavLink>
+              </li>
+              <li className="dasb-header-right-action">
+                <input type="checkbox" id="action" />
+                <label htmlFor="action">
+                  <img
+                    width={32}
+                    src="https://php.spruko.com/ynex/ynex/assets/images/faces/9.jpg"
+                    alt="Ảnh admin"
+                  />
+                  <span>Tuấn</span>
+                  <i>
+                    <FaChevronDown />
+                  </i>
+                </label>
+                <div className="dasb-header-right-action-drop" id="drop">
+                  <NavLink className="info" to="">
+                    <i></i>
+                    <p>Thông tin</p>
+                  </NavLink>
+                  <NavLink className="log" to="">
+                    <i></i>
+                    <p>Logout</p>
+                  </NavLink>
+                </div>
+              </li>
+            </ul>
+          </div>
+          <div className="dasb-wapper-main-bg">
+            <div className="content">{children}</div>
+          </div>
         </div>
       </div>
     </div>
