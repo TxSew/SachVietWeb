27235a30659d81a412b358958428d7220216f0cd
thanh-ukpc <thanh.do@ukparkingcontrol.com>
2023-11-20 08:54:13 +0700

    update base api

--- src/configs/BaseApi.tsx
+++ src/configs/BaseApi.tsx
@@ -1,8 +1,8 @@
 export const BaseAPi = {
-  // baseURL: "http://192.168.110.122:3000",
-  // baseURL: "http://192.168.110.73:8003",
-  // baseURL: "http://localhost:8005",
-  // baseURL: "https://9cd9-123-25-102-116.ngrok-free.app/",
-  // baseURL: "https://api.sachviet.click",
-  baseURL: process.env.REACT_APP_HTTP_API,
+    // baseURL: "http://192.168.110.122:3000",
+    // baseURL: "http://192.168.110.73:8003",
+    // baseURL: "http://localhost:8005",
+    // baseURL: "https://9cd9-123-25-102-116.ngrok-free.app/",
+    // baseURL: "https://api.sachviet.click",
+    baseURL: 'https://6118-14-241-150-19.ngrok-free.app',
 };
